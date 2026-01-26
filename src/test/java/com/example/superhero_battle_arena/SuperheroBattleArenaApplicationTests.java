89b30ab586d57d6638b69d2f15970f2908255912
Anyul Rivas <anyulled@gmail.com>
2026-01-26 19:58:57 +0100

    feat: Initialize Superhero Battle Arena Spring Boot application with Maven, Docker, and superhero dataset.

---
+++ src/test/java/com/example/superhero_battle_arena/SuperheroBattleArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superhero_battle_arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroBattleArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
